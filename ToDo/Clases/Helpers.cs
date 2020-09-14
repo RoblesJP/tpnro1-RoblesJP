77575b16ff068a17e22cc13f2df36770f33c35a3
RoblesJP <juan.robles1417@gmail.com>
2020-09-14 01:28:17 -0300

    Agregar clase Tarea y Helpers

---
+++ ToDo/Clases/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Clases
+{
+    static public class Helpers
+    {
+        static public Tarea BuscarId(List<Tarea> lista, int? id)
+        {
+            Tarea tarea = lista.Find(x => x.Id.Equals(id));
+            return tarea;
+        }
+
+        static public Tarea BuscarPalabraClave(List<Tarea> lista, string palabra)
+        {
+            Tarea tarea = lista.Find(x => x.Descripcion.Contains(palabra));
+            return tarea;
+        }
+    }
+}
---
+++ ToDo/Clases/Tarea.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Clases
+{
+    public enum EstadoTarea
+    {
+        Pendiente,
+        Realizada
+    }
+    public class Tarea
+    {
+        private int id;
+        private string descripcion;
+        private int duracion;
+        private EstadoTarea estado;
+
+        public int Id { get => id; set => id = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int Duracion { get => duracion; set => duracion = value; }
+        public EstadoTarea Estado { get => estado; set => estado = value; }
+
+
+
+        public Tarea(int id, string descripcion, int duracion)
+        {
+            Id = id;
+            Descripcion = descripcion;
+            Duracion = duracion;
+            Estado = EstadoTarea.Pendiente;
+        }
+
+        public void Realizar()
+        {
+            Estado = EstadoTarea.Realizada;
+        }
+        
+    }
+}
